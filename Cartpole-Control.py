98c4fe213d638b7b33541de1d7b0fdceb6e92df2
Jerry Foo <jiruif@hotmail.com>
2022-05-06 19:07:42 -0400

    second commit

--- Cartpole-Control.py
+++ Cartpole-Control.py
@@ -10,12 +10,15 @@ class Cartpole(Env):
 
     def __init__(self,
                  seed=1,
-                 m=1,
-                 L=1, b=1,
+                 mcart=1,
+                 mpole,
+                 L=1,
+                 b=1,
                  max_force=1):
         """
         :param seed: Random seed
-        :param m: mass of cart pole
+        :param mcart: mass of cart
+        :param mpole: mass of pole
         :param L: length of pole
         :param b: damping coefficient
         :param max_force: maximum action
@@ -29,21 +32,47 @@ class Cartpole(Env):
             a) F: horizontal force applied to the cart which range between (-max_force, max_force) [defined by user]
         """
         self.seed = seed
-        self.m = m
+        self.m_pole = mpole
+        self.m_cart = mcart
         self.L = L
+        self.rho = self.m_pole / self.L
         self.b = b
         self.g = 9.81
+        self.dt = 0.01
         self.max_force = max_force
-        self.state_bound = np.array([np.pi / 2, np.pi / 10, 100, 5], dtype=np.float32)
+        self.max_theta = np.pi / 2
+        self.max_thetad = np.pi/10
+        self.x_max = 100
+        self.v_max = 5
+
+        self.state_bound = np.array([self.max_theta, self.max_thetad, self.x_max, self.v_max], dtype=np.float32)
 
         self.action_space = Box(low=-self.max_force, high=self.max_force, shape=(1,), dtype=np.float32)
         self.observation_space = Box(low=-self.state_bound, high=self.state_bound)
-        self.t = 0
-        self.dt = 0.02
-        self.max_time = 12
+        self.t = None
+        self.max_time = None
+        self.steps_beyond_done = None
 
     def reset(self):
         super().reset(seed=self.seed)
-        self.init_theta =
+        self.state = self.np_random.uniform(low=-0.2, high=0.2, size=(4,))
+        self.t = 0
+        self.steps_beyond_done = None
+        self.max_time = 120
+        return np.array(self.state, dtype=np.float32)
+
+    def step(self, action):
+        theta, thetaot, x, xdot = self.state
+        force = np.clip(action, -self.max_force, self.max_force)[0]
+        self.last_Force = force
+        costheta = math.cos(theta)
+        sintheta = math.sin(theta)
+        temp = (
+            force + self.rho * thetaot ** 2 * sintheta
+        ) / (self.m_pole + self.m_cart)
+        thetaacc = (self.g * sintheta)
+
+
+
 
 
